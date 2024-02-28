cca4ada0ec0d31b9d7f67fc6bc0876be8e926b79
Shreya Patil <shreyapatiljnvt@gmail.com>
2024-02-28 23:21:34 +0530

    another changes in userStories

--- src/models/userStoryModel.js
+++ src/models/userStoryModel.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 
 const userStorySchema = new mongoose.Schema({
   story_id: { type: Number, required: true },
-  user_id: { type: Number, required: true },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  },
   media_url: { type: String, required: true },
   caption: { type: String, required: true },
   date: { type: Date, required: true },
