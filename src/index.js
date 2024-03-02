6bca721a8c7557a00fdb46e5ffc477f43f7a99e0
Asmita Mulkutkar <asmitamulkutkar16@gmail.com>
2024-03-02 13:28:37 +0530

    add CORS

--- src/index.js
+++ src/index.js
@@ -2,32 +2,39 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 dotenv.config({ path: "./env" });
 //2nd method for connecting db
 connectDB();
 
 import userRoutes from "./routes/userRoutes.js";
 import feedbackRoutes from "./routes/FeedbackRoutes.js";
-import serviceProviderRoutes from './routes/serviceProviderRoutes.js';
-import serviceRequestRoutes from './routes/serviceRequestRoutes.js';
-import cookieParser from 'cookie-parser';
-import userStoryRoutes from './routes/userStoryRoutes.js';
-import express from 'express';
+import serviceProviderRoutes from "./routes/serviceProviderRoutes.js";
+import serviceRequestRoutes from "./routes/serviceRequestRoutes.js";
+import cookieParser from "cookie-parser";
+import userStoryRoutes from "./routes/userStoryRoutes.js";
+import express from "express";
 const app = express();
 
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(
+    cors({
+        origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+    })
+);
 
 //Use routes
 
-
-app.use('/user-stories', userStoryRoutes);
+app.use("/user-stories", userStoryRoutes);
 app.use("/users", userRoutes);
 app.use("/service-providers", serviceProviderRoutes);
 app.use("/feedback", feedbackRoutes);
-app.use("/service-req",serviceRequestRoutes);
+app.use("/service-req", serviceRequestRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
